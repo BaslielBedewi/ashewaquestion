4e3bb78b8073a02367deeb746bd45cad42be95f1
Wasehaile <104649119+Wasehaile@users.noreply.github.com>
2022-08-20 06:46:14 -0700

    notification started

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import ashewalogo from "../assets/ashewaLogo.png";
 import { Link } from "react-router-dom";
-
+import { FaBell } from "@react-icons/all-files/fa/FaBell";
+import { IconContext } from "react-icons";
 function Nav() {
   return (
     <>
@@ -26,6 +27,9 @@ function Nav() {
             Table
           </Link>
         </div>
+        <IconContext.Provider value={{color:"blue"}} >
+         <FaBell />
+         </IconContext.Provider>
         <Link to={"/"} className="pr-7 text-dim hover:text-primary">
           Logout
         </Link>
